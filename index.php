eee1d0f2e152b67281706b6ed2cfd6836ba870ff
Hana Choi <hana1056@naver.com>
2016-01-13 13:56:55 +0900

    #4 css 코드 추가

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
+<style>
+  /* css는 c 스타일의 주석을 사용 */
+  b { color: red;
+      font-weight: normal;
+      background-color: #cccccci;
+    }
+
+  #hellobox {
+      width: 500px;
+      height: 300px;
+      border: 1px solid #ff0000;
+      margin: 10px;
+      position: absolute;
+      top: 20px;
+      right: 20px;
+    }
+
+  .nicename {
+      font-weight: bold;
+      font-size: 20px;
+      text-decoration: underline;
+      cursor: pointer;
+    }
+</style>
+
+<div id="hellobox">
+   Good morning, Lady?
+</div>
+
 <?php
 
 for( $i = 0; $i < 1000; $i ++){
-  echo $i . "Hello, Hana Choi!!<br >";
+  echo $i . ", <b>Hello</b>, <span class='nicename'>Hana Choi</span>!!<br >";
 }
 
 ?>
