0a1816ee24356c884bd9ca577f93351803d13ccd
Hana Choi <hana1056@naver.com>
2016-01-14 15:34:28 +0900

    #6 POST 작동 연습

---
+++ postForm.php
@@ -0,0 +1,12 @@
+<form method="post" action="postResult.php">
+  <h1>For you, VOMVOM service</h1>
+  Input your name<br >
+  <input type = "text" name="name" id="name" values="">
+  <br>
+  Input your birthday(ex:1998 08 07)<br >
+  <input type = "text" name="year" id="year" values="">
+  <input type = "text" name="mon" id="mon" values="">
+  <input type = "text" name="day" id="day" values="">
+  <br>
+  <input type = "submit" value="View My Result">
+</form>
